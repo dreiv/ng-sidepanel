f90ea7012c98413e862f4fbacbd0a606a462aa60
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-08-29 15:08:46 +0300

    added footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { FillerComponent } from './filler/filler.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    FillerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/filler/filler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filler',
+  templateUrl: './filler.component.html',
+  styleUrls: ['./filler.component.css']
+})
+export class FillerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
