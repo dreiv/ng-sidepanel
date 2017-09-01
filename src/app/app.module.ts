daba129da11caceb1b8aa84fdb309aea3ad6cc01
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-09-01 09:59:28 +0300

    refactored footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FillerComponent } from './filler/filler.component';
-import { FooterComponent } from './footer/footer.component';
 import { SidepanelModule } from './modules/sidepanel/sidepanel.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     FillerComponent,
   ],
   imports: [
